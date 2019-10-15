223117ec5b9eee8a38404ca4a6d84aacdd9469ca
Jonathan Frees <jmfrees@uab.edu>
2019-10-15 14:48:41 -0500

    Add unit tests for containers

---
+++ src/test/java/farm/dashboard/ItemContainerTest.java
@@ -0,0 +1,83 @@
+package farm.dashboard;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+public class ItemContainerTest {
+
+    ItemContainer itemContainer;
+
+    @Before
+    public void setUp(){
+       itemContainer = new ItemContainer();
+    }
+
+    @Test
+    public void testSetName(){
+        assertNull(itemContainer.getName());
+        itemContainer.setName("Name 1");
+        assertNotNull("ItemContainer name was not set", itemContainer.getName());
+        assertEquals(itemContainer.getName(), "Name 1");
+    }
+
+    @Test
+    public void testSetPrice(){
+        assertEquals(itemContainer.getPrice(), 0.0, 0.1);
+        itemContainer.setPrice(3.14);
+        assertNotNull("ItemContainer price was not set", itemContainer.getPrice());
+        assertEquals(itemContainer.getPrice(), 3.14, 0.001);
+    }
+
+    @Test
+    public void testSetLocationX(){
+        assertEquals(itemContainer.getLocationX(), 0);
+        itemContainer.setLocationX(3);
+        assertNotNull("ItemContainer X location was not set", itemContainer.getLocationX());
+        assertEquals(itemContainer.getLocationX(), 3);
+    }
+
+    @Test
+    public void testSetLocationY(){
+        assertEquals(itemContainer.getLocationY(), 0);
+        itemContainer.setLocationY(6);
+        assertNotNull("ItemContainer Y location was not set", itemContainer.getLocationY());
+        assertEquals(itemContainer.getLocationY(), 6);
+    }
+
+    @Test
+    public void testSetPosition(){
+        int originalX = itemContainer.getLocationX();
+        int originalY = itemContainer.getLocationY();
+        itemContainer.setPosition(8, 10);
+        assertNotEquals(originalX, itemContainer.getLocationX());
+        assertNotEquals(originalY, itemContainer.getLocationY());
+        assertEquals(itemContainer.getLocationX(), 8);
+        assertEquals(itemContainer.getLocationY(), 10);
+    }
+
+    @Test
+    public void testSetLength(){
+        assertEquals(itemContainer.getLength(), 0);
+        itemContainer.setLength(7);
+        assertNotNull("ItemContainer X location was not set", itemContainer.getLength());
+        assertEquals(itemContainer.getLength(), 7);
+    }
+
+    @Test
+    public void testSetWidth(){
+        assertEquals(itemContainer.getWidth(), 0);
+        itemContainer.setWidth(2);
+        assertNotNull("ItemContainer X location was not set", itemContainer.getWidth());
+        assertEquals(itemContainer.getWidth(), 2);
+    }
+
+    @Test
+    public void testSetHeight(){
+        assertEquals(itemContainer.getHeight(), 0);
+        itemContainer.setHeight(18);
+        assertNotNull("ItemContainer X location was not set", itemContainer.getHeight());
+        assertEquals(itemContainer.getHeight(), 18);
+    }
+
+}
---
+++ src/test/java/farm/dashboard/ItemTest.java
@@ -0,0 +1,88 @@
+package farm.dashboard;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+public class ItemTest {
+
+    Item item;
+
+    @Before
+    public void setUp(){
+       item = new Item();
+    }
+
+    @Test
+    public void testSetName(){
+        assertNull(item.getName());
+        item.setName("Name 1");
+        assertNotNull("Item name was not set", item.getName());
+        assertEquals(item.getName(), "Name 1");
+    }
+
+    @Test
+    public void testSetPrice(){
+        assertEquals(item.getPrice(), 0.0, 0.1);
+        item.setPrice(3.14);
+        assertNotNull("Item price was not set", item.getPrice());
+        assertEquals(item.getPrice(), 3.14, 0.001);
+    }
+
+    @Test
+    public void testSetLocationX(){
+        assertEquals(item.getLocationX(), 0);
+        item.setLocationX(3);
+        assertNotNull("Item X location was not set", item.getLocationX());
+        assertEquals(item.getLocationX(), 3);
+    }
+
+    @Test
+    public void testSetLocationY(){
+        assertEquals(item.getLocationY(), 0);
+        item.setLocationY(6);
+        assertNotNull("Item Y location was not set", item.getLocationY());
+        assertEquals(item.getLocationY(), 6);
+    }
+
+    @Test
+    public void testSetPosition(){
+        int originalX = item.getLocationX();
+        int originalY = item.getLocationY();
+        item.setPosition(8, 10);
+        assertNotEquals(originalX, item.getLocationX());
+        assertNotEquals(originalY, item.getLocationY());
+        assertEquals(item.getLocationX(), 8);
+        assertEquals(item.getLocationY(), 10);
+    }
+
+    @Test
+    public void testSetLength(){
+        assertEquals(item.getLength(), 0);
+        item.setLength(7);
+        assertNotNull("Item X location was not set", item.getLength());
+        assertEquals(item.getLength(), 7);
+    }
+
+    @Test
+    public void testSetWidth(){
+        assertEquals(item.getWidth(), 0);
+        item.setWidth(2);
+        assertNotNull("Item X location was not set", item.getWidth());
+        assertEquals(item.getWidth(), 2);
+    }
+
+    @Test
+    public void testSetHeight(){
+        assertEquals(item.getHeight(), 0);
+        item.setHeight(18);
+        assertNotNull("Item X location was not set", item.getHeight());
+        assertEquals(item.getHeight(), 18);
+    }
+
+    @Test public void testClassExists() {
+        DashboardApp classUnderTest = new DashboardApp();
+        assertNotNull("You really must have messed up", 1);
+    }
+
+}
