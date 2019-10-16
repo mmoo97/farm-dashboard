d3812e2c07579d5cea95ad823d01c5a2e1874608
Mitchell Moore <mitchell1484@gmail.com>
2019-10-15 20:14:50 -0500

    Add common imports

---
+++ src/main/java/farm/dashboard/DroneAnimation.java
@@ -0,0 +1,26 @@
+package farm.dashboard;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.SequentialTransition;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+
+public class DroneAnimation extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
