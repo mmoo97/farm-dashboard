56528b0c95f7584d21e777aded5a064b67bfb53f
Curt Wilson <curtisrw@uab.edu>
2019-11-04 16:11:03 -0600

    Added MarketVisitor Class

---
+++ src/main/java/farm/dashboard/MarketVisitor.java
@@ -0,0 +1,14 @@
+package farm.dashboard;
+
+class MarketVisitor extends Visitor {
+
+    @java.lang.Override
+    public void visitItemContainer(ItemContainer itemContainer) {
+
+    }
+
+    @java.lang.Override
+    public void visitItem(Item item) {
+
+    }
+}
